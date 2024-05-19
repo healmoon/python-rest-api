76459c527c4992bda540dad64eb3758daa8902d5
healmoon <166487751+healmoon@users.noreply.github.com>
2024-05-19 12:29:56 +0300

    Add files via upload

---
+++ eshop/view/product_schemas.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields
+
+
+class ProductSchema(Schema):
+    id = fields.String()
+    name = fields.String()
+    price = fields.Float()
+
+
+class ProductGetManyParams(Schema):
+    page = fields.Int(required=True)
+    limit = fields.Int(required=True)
